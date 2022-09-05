b216e4af92855834458776cf5902a008a20c1646
root <jyothikumari2610@gmail.com>
2022-09-05 05:59:43 +0000

    tastykitchen

---
+++ src/components/Home/index.js
@@ -0,0 +1,16 @@
+import Header from '../Header'
+
+import MenuSection from '../MenuSection'
+import Footer from '../Footer'
+
+import './index.css'
+
+const Home = () => (
+  <>
+    <Header />
+    <MenuSection />
+    <Footer />
+  </>
+)
+
+export default Home
